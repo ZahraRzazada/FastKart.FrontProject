92b498d06a4af6754aa398bc2a4b918a7bca0533
ZahraRzazada <zahraer@code.edu.az>
2023-10-07 13:14:58 +0400

    4 parts are ready ticr

--- js/app.js
+++ js/app.js
@@ -95,3 +95,37 @@ $('.slider1').slick({
       }
     ]
   });
+  $('.slider3').slick({
+    dots: false,
+    infinite: true,
+    arrow:false,
+    autoplay:true,
+    speed: 300,
+    slidesToShow: 5 ,
+    slidesToScroll: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
