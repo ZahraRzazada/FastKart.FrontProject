fe9bc7a2e6007f6e0f38b461f9818a40226281cf
ZahraRzazada <zahraer@code.edu.az>
2023-10-12 00:38:37 +0400

    About end,detail 50%

--- js/app.js
+++ js/app.js
@@ -462,3 +462,37 @@ $('.slider1').slick({
       }
     ]
   });
+  $('.slider14').slick({
+    dots: true,
+    infinite: true,
+    arrow:false,
+    autoplay:true,
+    speed: 300,
+    slidesToShow: 6 ,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
